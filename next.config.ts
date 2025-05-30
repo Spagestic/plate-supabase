aa1468d4737b1c57eed89ab947bd5dceb6ccb5d0
spagestic <steelmax666@gmail.com>
2025-05-31 07:10:20 +0800

    fix: resolve Yjs multiple imports issue for webpack and turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Fix Yjs multiple imports issue for webpack
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      yjs: "yjs",
+    };
+
+    return config;
+  },
+  turbopack: {
+    resolveAlias: {
+      // Fix Yjs multiple imports issue for turbopack
+      yjs: "yjs",
+    },
+  },
 };
 
 export default nextConfig;
