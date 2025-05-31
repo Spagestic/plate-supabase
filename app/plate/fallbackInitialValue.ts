25f6ba1b5e5e969676790c10c4ae87fef1d0884f
spagestic <steelmax666@gmail.com>
2025-05-31 17:44:41 +0800

    refactor: simplify fallback initial value by removing unnecessary elements

--- app/plate/fallbackInitialValue.ts
+++ app/plate/fallbackInitialValue.ts
@@ -1,14 +1,8 @@
 import type { Value } from "@udecode/plate";
 
 export const fallbackInitialValue: Value = [
-  { type: "h3", children: [{ text: "Title" }] },
-  { type: "blockquote", children: [{ text: "This is a quote." }] },
   {
     type: "p",
-    children: [
-      { text: "With some " },
-      { text: "bold", bold: true },
-      { text: " text for emphasis!" },
-    ],
+    children: [{ text: "" }],
   },
 ];
